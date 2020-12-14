5a854b4b8c8df3c3d80362dfdf5bce0f25f6c4a0
bhargav.sarvaria <bhargav.sarvaria@gmail.com>
2020-12-14 18:13:28 +0530

    My first assignment

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,9 +1,34 @@
  (function(){
 'use-strict';
 
-   angular.module('myFirstApp',[])
-   .controller('MyFirstController', function($scope){
-     $scope.name = 'Bhargav';
-   })
+   angular.module('LunchCheck',[])
+   .controller('LunchCheckController', LunchCheckController);
 
+   LunchCheckController.$inject = ['$scope'];
+   function LunchCheckController($scope){
+     $scope.message = '';
+     $scope.inputText = '';
+
+     $scope.generate = function(){
+       var foodArray = $scope.inputText.split(',')
+       if(isBlank($scope.inputText)){
+         $scope.message = 'Please enter data first';
+       }else if(foodArray.length > 0 && foodArray.length <= 3){
+         $scope.message = 'Enjoy!';
+       }else if(foodArray.length > 3){
+         $scope.message = 'Too much!';
+       }
+     }
+
+     var isBlank = function(text){
+       if(text == null){
+         return true;
+       }else if(text == ''){
+         return true;
+       }else{
+         return false;
+       }
+     }
+
+   }
  })();
